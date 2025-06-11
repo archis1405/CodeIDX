1ecd5c5c5201d2599b60e58859dd517c9e5c961e
archis1405 <archismandas1405@gmail.com>
2025-06-12 00:31:15 +0530

    basic project setup commit

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+import {PORT} from './config/serverConfig.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/ping', (req, res) => {
+  return res.json({message : 'Server is running'});
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
